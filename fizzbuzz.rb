b715141e4c9938df4719b3940a53aeed462c1151
Rick Rosetti <rick.rosetti@gmail.com>
2019-03-31 19:54:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,16 +5,14 @@ def fizzbuzz(n)
 end
 
 
-=begin
-
-if (n % 3 == 0 && n % 5 == 0)
+if n % 3 == 0 && n % 5 == 0
   return "FizzBuzz"
-elsif (n % 3 == 0)
+elsif n % 3 == 0
   return "Fizz"
-elsif (n % 5 == 0)
+elsif n % 5 == 0
   return "Buzz"
 else
   return nil
-=end
+
 
   
